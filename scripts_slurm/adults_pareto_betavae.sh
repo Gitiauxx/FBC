d78773cf9bf02cd93c4e54e06beb164119378c0c
gitiauxx <xavier.gitiaux@gmail.com>
2020-11-08 16:28:05 -0500

    First commit

---
+++ scripts_slurm/adults_pareto_betavae.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#SBATCH --job-name=adults_eo
+#SBATCH --output=/scratch/xgitiaux/adults_eobv_%j_%a.out
+#SBATCH --error=/scratch/xgitiaux/adults_eobv_%j_%a.error
+#SBATCH --mail-user=xgitiaux@gmu.edu
+#SBATCH --mail-type=END
+#SBATCH --export=ALL
+#SBATCH --partition=all-HiPri
+#SBATCH --nodes 1
+#SBATCH --mem=4G
+#SBATCH --tasks 1
+#SBATCH --qos=csqos
+#SBATCH --array=0-50
+
+echo $SLURM_ARRAY_TASK_ID
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 0
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.2
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.4
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.6
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.8
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 1.0
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 1.2
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 1.4
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 1.6
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 1.8
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 2.0
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 2.2
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 2.4
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 2.6
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 2.8
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 3.0
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 3.2
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 3.4
+../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_betavae.yml --seed $SLURM_ARRAY_TASK_ID --beta 3.6
+
