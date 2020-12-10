a462c57231faf26749ffa3181c9059cc10e52f17
gitiauxx <xavier.gitiaux@gmail.com>
2020-12-10 08:43:35 -0500

    Add regressive method

--- scripts_slurm/adults_pareto_comp.sh
+++ scripts_slurm/adults_pareto_comp.sh
@@ -4,22 +4,7 @@
 #SBATCH --output=/scratch/xgitiaux/adults_eocomp_%j_%a.out
 #SBATCH --error=/scratch/xgitiaux/adults_eocomp_%j_%a.error
 #SBATCH --mail-user=xgitiaux@gmu.edu
-#SBATCH --mail-type=../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.025
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.05
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.075
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc_reg.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.1
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc._regyml --seed $SLURM_ARRAY_TASK_ID --beta 0.125
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.15
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.175
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.2
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.225
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.275
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.3
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.325
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.35
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.375
-../fvae-env/bin/python3 eval.py --config_path configs/adults/adults_pareto_fbc.yml --seed $SLURM_ARRAY_TASK_ID --beta 0.4
-END
+#SBATCH --mail-type=END
 #SBATCH --export=ALL
 #SBATCH --partition=all-LoPri
 #SBATCH --nodes 1
